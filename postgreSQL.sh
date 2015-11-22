e3de519373804bf30c31a0cc0f516e8c83e28feb
god <Iamhell@fuckyou.com>
2015-11-22 19:46:29 +0000

    fixed so would run out of the bash script

--- postgreSQL.sh
+++ postgreSQL.sh
@@ -5,16 +5,4 @@ sudo pg_createcluster 9.3 zoopla
 #default login is postgres
 sudo -su postgres
 #sql code begins here
-psql
-
-CREATE DATABASE zooplatest;
--- not sure about this command below
-CREATE SCHEMA zooplalive;
-
-CREATE USER externalAPI PASSWORD 'test';
-
-GRANT ALL ON SCHEMA zooplalive TO externalAPI;
-
-CREATE TABLE zoopladump ( listing_id varchar(10), price varchar(10), beds varchar(10), latitude varchar(10), longitude varchar(10));
-
-CREATE TABLE zooplaclean ( listing_id varchar(10), price float, beds int, latitude float, longitude float);
+psql -f postgreSQLsetup.sql
